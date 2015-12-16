b631576315c51e063024fc879d91bdc2532e578c
Maryam <maryam.sarab@gmail.com>
2015-12-15 21:43:06 -0600

    fix the index.js

--- D3/index.js
+++ D3/index.js
@@ -1,11 +1,8 @@
 //$(document).ready(function(){
-	d3.csv("data.csv",function(myArrayOfObjects){
-		
-
-		myArrayOfObjects.forEach(function(d){
-
-			console.log(d.x +" ,"+ d.y);
-		});
-	});
+	d3.csv("data.csv", function (myArrayOfObjects){
+        myArrayOfObjects.forEach(function (d){
+          console.log(d.x + ", " + d.y);
+        });
+      });
 
 //});
