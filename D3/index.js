4a5db6727a631a1c369fa093169a2a5c5ef8597b
Maryam <maryam.sarab@gmail.com>
2015-12-15 21:32:38 -0600

    fix the index.js

--- D3/index.js
+++ D3/index.js
@@ -1,7 +1,6 @@
 //$(document).ready(function(){
 	d3.csv("data.csv",function(myArrayOfObjects){
-		var d3 = require("d3"),
-    jsdom = require("jsdom");
+		
 
 var document = jsdom.jsdom(),
     svg = d3.select(document.body).append("svg");
